63a1991e7c957e177f9a5f7620454b1c2bee0c2c
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-07-30 13:15:43 -0500

    added local storage variable to store history

--- assets/script.js
+++ assets/script.js
@@ -8,26 +8,59 @@ var cardHumidityEl = $('#card-humidity');
 var cardUVIndexEl = $('#card-uv-index');
 var cardDateEl = $('#card-date');
 var cardIconEl = $('#card-icon');
+var dayListEl = $('#day-list');
+var historyItemEl;
+var historyArray = [];
+var linkName;
+
+if(JSON.parse(localStorage.getItem('history')) != null){
+    historyArray = JSON.parse(localStorage.getItem('history'));
+    for(var i = 0; i < historyArray.length; i = i + 2){
+        var liEl = $('<li class="history-item" id="' + historyArray[i+1] + '"></li>');
+        liEl.html(historyArray[i]);
+        historyListEl.append(liEl);
+        historyItemEl = $('.history-item');
+    }
+}
+
+
 
 searchBtnEl.on('click', function(event) {
     event.preventDefault();
-    addToHistory();
-    getWeather();
+    getWeather(cityInput.eq(0).val().trim());
+    addToHistory(cityInput.val().trim());
+    cityInput.val('');
 })
 
-function addToHistory() {
-    var liEl = $('<li></li>');
-    liEl.html(cityInput.val());
-    historyListEl.append(liEl);
+function addToHistory(name) {
+    var liEl = $('<li class="history-item" id="' + linkName + '"></li>');
+    liEl.html(name);
+
+    if(historyArray.includes(name)){
+        return;
+    } else {
+        historyListEl.append(liEl);
+        historyItemEl = $('.history-item');
+        historyArray.push(name);
+        historyArray.push(linkName);
+        localStorage.setItem('history', JSON.stringify(historyArray));
+    }
+    
+
+    historyItemEl.on('click', function(event) {
+        event.preventDefault();
+        getWeather(this.id);
+    })
 }
+
+
 
 String.prototype.replaceAt = function(index, replacement) {
         return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 }
 
-function getWeather() {
-    var inputCityName = cityInput.eq(0).val().trim();
-    var linkName = inputCityName;
+function getWeather(inputCityName) {
+    linkName = inputCityName;
     for(var i = 0; i < linkName.length; i++){
         if(linkName.charAt(i) == ' '){
             linkName = linkName.replaceAt(i+1,  linkName.charAt(i+1).toLowerCase());
@@ -50,6 +83,7 @@ function getWeather() {
                 .then (function (data) {
                     console.log(data);
                     showWeather(data);
+                    showFutureWeather(data);
                 });
         });
 
@@ -67,6 +101,20 @@ function getWeather() {
             cardUVIndexEl.css('background-color', 'yellow');
         } else {
             cardUVIndexEl.css('background-color', 'green');
+        }
+    }
+
+    function showFutureWeather(data) {
+        dayListEl.html('');
+        for(var i = 1; i < 6; i++){
+            var liEl = $('<li id="a-future-day"></li>');
+            var date = data.daily[i].sunset;
+            liEl.append('<h2>' + moment(date, 'X').format('M/D/YYYY') + '</h2>')
+            liEl.append('<img id="five-icon" src="http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '@2x.png">')
+            liEl.append('<h3>Temp: ' + data.daily[i].temp.day + '<span>&#8457;</span></h3>')
+            liEl.append('<h3>Wind: ' + data.daily[i].wind_speed + ' MPH</h3>')
+            liEl.append('<h3>Humidity: ' + data.daily[i].humidity + '%</h3>')
+            dayListEl.append(liEl);
         }
     }
 }
