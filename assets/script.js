97d5d329e1825896511628cb83c9f02c9e5bb754
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-07-31 09:18:52 -0500

    fixing issue with GitHub Page

--- assets/script.js
+++ assets/script.js
@@ -12,6 +12,7 @@ var dayListEl = $('#day-list');
 var historyItemEl;
 var historyArray = [];
 var linkName;
+var itemFound = false;
 
 function addToHistory(name) {
     var liEl = $('<li class="history-item" id="' + linkName + '"></li>');
@@ -22,18 +23,27 @@ function addToHistory(name) {
     } else {
         historyListEl.append(liEl);
         historyItemEl = $('.history-item');
+        itemFound = true;
         historyArray.push(name);
         historyArray.push(linkName);
         localStorage.setItem('history', JSON.stringify(historyArray));
     }
-    historyItemEl.on('click', function(event) {
+    historyItemEl.on('click', function(event){
         event.preventDefault();
         getWeather(this.id);
-    })
+    });
 }
 
-
-
+function init() {
+    if($('.history-item') !== undefined){
+        $('.history-item').on('click', function(event){
+            event.preventDefault();
+            getWeather(this.id);
+        });
+    }
+}
+init();
+    
 String.prototype.replaceAt = function(index, replacement) {
         return this.substring(0, index) + replacement + this.substring(index + replacement.length);
 }
@@ -60,45 +70,41 @@ function getWeather(inputCityName) {
                     return response.json();
                 })
                 .then (function (data) {
-                    console.log(data);
-                    showWeather(data);
+                    showWeather(data, inputCityName);
                     showFutureWeather(data);
                 });
-        });
-
-    function showWeather(data) {
-        cardTitleEl.html(inputCityName);
-        cardDateEl.html(moment().format('M/D/YYYY'));
-        cardIconEl.attr('src', 'http://openweathermap.org/img/wn/' + data.current.weather[0].icon + '@2x.png')
-        cardTempEl.html(data.current.temp);
-        cardWindEl.html(data.current.wind_speed);
-        cardHumidityEl.html(data.current.humidity);
-        cardUVIndexEl.html(data.current.uvi);
-        if(data.current.uvi >= 8){
-            cardUVIndexEl.css('background-color', 'red');
-        } else if (data.current.uvi >= 3){
-            cardUVIndexEl.css('background-color', 'yellow');
-        } else {
-            cardUVIndexEl.css('background-color', 'green');
-        }
-    }
-
-    function showFutureWeather(data) {
-        dayListEl.html('');
-        for(var i = 1; i < 6; i++){
-            var liEl = $('<li id="a-future-day"></li>');
-            var date = data.daily[i].sunset;
-            liEl.append('<h2>' + moment(date, 'X').format('M/D/YYYY') + '</h2>')
-            liEl.append('<img id="five-icon" src="http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '@2x.png">')
-            liEl.append('<h3>Temp: ' + data.daily[i].temp.day + '<span>&#8457;</span></h3>')
-            liEl.append('<h3>Wind: ' + data.daily[i].wind_speed + ' MPH</h3>')
-            liEl.append('<h3>Humidity: ' + data.daily[i].humidity + '%</h3>')
-            dayListEl.append(liEl);
-        }
-        $('.custom-col').css('height', 'fit-content');
+        });    
+}
+function showWeather(data, inputCityName) {
+    cardTitleEl.html(inputCityName);
+    cardDateEl.html(moment().format('M/D/YYYY'));
+    cardIconEl.attr('src', 'http://openweathermap.org/img/wn/' + data.current.weather[0].icon + '@2x.png')
+    cardTempEl.html(data.current.temp);
+    cardWindEl.html(data.current.wind_speed);
+    cardHumidityEl.html(data.current.humidity);
+    cardUVIndexEl.html(data.current.uvi);
+    if(data.current.uvi >= 8){
+        cardUVIndexEl.css('background-color', 'red');
+    } else if (data.current.uvi >= 3){
+        cardUVIndexEl.css('background-color', 'yellow');
+    } else {
+        cardUVIndexEl.css('background-color', 'green');
     }
 }
-
+function showFutureWeather(data) {
+    dayListEl.html('');
+    for(var i = 1; i < 6; i++){
+        var liEl = $('<li id="a-future-day"></li>');
+        var date = data.daily[i].sunset;
+        liEl.append('<h2>' + moment(date, 'X').format('M/D/YYYY') + '</h2>')
+        liEl.append('<img id="five-icon" src="http://openweathermap.org/img/wn/' + data.daily[i].weather[0].icon + '@2x.png">')
+        liEl.append('<h3>Temp: ' + data.daily[i].temp.day + '<span>&#8457;</span></h3>')
+        liEl.append('<h3>Wind: ' + data.daily[i].wind_speed + ' MPH</h3>')
+        liEl.append('<h3>Humidity: ' + data.daily[i].humidity + '%</h3>')
+        dayListEl.append(liEl);
+    }
+    $('.custom-col').css('height', 'fit-content');
+}
 if(JSON.parse(localStorage.getItem('history')) != null){
     historyArray = JSON.parse(localStorage.getItem('history'));
     for(var i = 0; i < historyArray.length; i = i + 2){
